6dd2d6519bea8d1621827695433a93686db86565
artorias <agustincastro,irb@gmail.com>
2022-10-17 01:49:36 -0300

    Modificacion de dimenciones de div, section estetica de botones y canvas creacion de la funcion cambiar pantalla

---
+++ src/js/cambioDePantalla.js
@@ -0,0 +1,9 @@
+
+
+const ahorcado = document.querySelector(".sec-ahorcado");
+const inicio = document.querySelector(".sec-inicio-del-juego");
+
+function iniciarJuego() {
+    inicio.style.display = "none";
+    ahorcado.style.display = "flex";
+}
