8af6b632a3f10449d211ffd549a21c01d09fd743
artorias <agustincastro,irb@gmail.com>
2023-04-04 22:01:44 -0300

    recfactorisar

--- src/js/cambioDePantalla.js
+++ src/js/cambioDePantalla.js
@@ -1,76 +1,47 @@
-import { dibujarGiones, horca, letras } from "./ahorcado.js";
-import { palabraSecreta } from "./palabra.js";
+import { horca, incorrecto, dibujarGiones, letras } from "./ahorcado.js";
+import { palabraSecreta, validarTecla} from "./palabra.js";
 
-const ahorcado = document.querySelector(".sec-ahorcado");
-const inicio = document.querySelector(".sec-inicio-del-juego");
-const agregar = document.querySelector(".sec-agregar-palabra");
-const btnInicio = document.querySelector(".btn-iniciar-juego");
-const btnAgregar = document.querySelector(".btn-agregar-nueva-palabra");
-const btnReinicio = document.querySelector(".btn-nuevo-juego");
-const btnDesistir = document.querySelector(".btn-desistir");
-const btnGuardar = document.querySelector(".btn-guardar-empezar");
-const btnCancelar = document.querySelector(".btn-cancelar");
+const cambiarSec = (sacar, poner) => {
+  sacar.style.display = "none";
+  poner.style.display = "flex";
+};
 
-btnInicio.addEventListener("click", () => {
-    inicio.style.display = "none";
-    ahorcado.style.display = "flex";
-    let palabra = palabraSecreta();
-    console.log(palabra);
-    horca();
-    document.addEventListener("keydown", (event) => {// porque no funciona en otro modulo por ejeplo tener este evento en otrafucion en otro modulo llamar a la funcion y que registre los eventos
-        console.log(event.key);
-        let mayuscula = event.key.toUpperCase()
-        let minuscula = event.key.toLowerCase()
+const iniciarJuego = () => {
+  const palabra = palabraSecreta();
+  let intentos = 0;
+  const letrasPrecionadas = [];
+  let letrasCorrectas = "";
 
-        if (palabra.match(mayuscula) || palabra.match(minuscula)) {
-            for (let i = 0; i < palabra.length; i++) {
-                console.log("dentro del for");
-
-                if (palabra[i] === mayuscula) {
-                    console.log("coincide mayuscula");
-                    
-                    letras(palabra, mayuscula, i);
-                } else if (palabra[i] === minuscula) {//no si hay una forma mejor de comprobar si es minucula y que dibuje lo que correspanda
-                    console.log("coincide minucula");
-                    
-                    letras(palabra, minuscula, i);
-                    
-                }
-                console.log("abajo de la coindicion del for");
-            }
-            console.log("fuera del for");
-        } else {
-            console.log("nop");
-            
+  horca();
+  dibujarGiones(palabra);
+  document.addEventListener("keydown", (event) => {// porque no funciona en otro modulo por ejeplo tener este evento en otrafucion en otro modulo llamar a la funcion y que registre los eventos
+    if (!letrasPrecionadas.includes(event.key) && event.key.length === 1 && /^[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]+$/.test(event.key)) {
+      let mayuscula = event.key.toUpperCase();
+      let minuscula = event.key.toLowerCase();
+      letrasPrecionadas.push(event.key);
+      let tr = validarTecla(palabra, mayuscula, minuscula);
+      if (tr) {
+        for (let i = 0; i < palabra.length; i++) {
+          if (palabra[i] === mayuscula) {
+            letras(palabra, mayuscula, i, 460);
+            letrasCorrectas = letrasCorrectas + mayuscula;
+          } else if (palabra[i] === minuscula) {
+            letras(palabra, minuscula, i, 460);
+            letrasCorrectas = letrasCorrectas + minuscula;
+          }
+          /*no si hay una forma mejor de comprobar si es minucula y que dibuje lo que correspanda*/ 
         }
-        console.log("adios");
-    })
+        if (letrasCorrectas.length === palabra.length) {
+          alert("Ganaste");
+        }
+      } else {
+        intentos++;
+        letras(palabra, event.key , intentos, 300);
+        incorrecto(intentos); 
+      }
+    }
+  });
+  
+};
 
-});
-
-
-btnAgregar.addEventListener("click", () => {
-    inicio.style.display = "none";
-    agregar.style.display = "flex";
-});
-
-btnReinicio.addEventListener("click", () => {
-
-});
-
-btnDesistir.addEventListener("click", () => {
-    inicio.style.display = "flex";
-    ahorcado.style.display = "none";
-});
-
-btnGuardar.addEventListener("click", () => {
-
-});
-
-btnCancelar.addEventListener("click", () => {
-    inicio.style.display = "flex";
-    agregar.style.display = "none";
-});
-
-
-export {agregar, inicio, ahorcado};
+export {cambiarSec, iniciarJuego};
--- src/js/palabra.js
+++ src/js/palabra.js
@@ -1,19 +1,28 @@
-import { dibujarGiones } from "./ahorcado.js";
-import { ahorcado } from "./cambioDePantalla.js";
 import { palabras } from "./db.js";
 
 const palabraSecreta = () => {
+  const numeroDePalabras = palabras.length;
+  const generadorAleatorio = () => Math.floor(Math.random() * numeroDePalabras);
+    
+  let palabraAleatoria = palabras[generadorAleatorio()];
+  console.log(palabraAleatoria);
 
-    const letrasPresionadas = [];
-    
-    const numeroDePalabras = palabras.length;
-    const generadorAleatorio = () => Math.floor(Math.random() * numeroDePalabras);
-    
-    const palabraAleatoria = palabras[generadorAleatorio()];
-    
-    dibujarGiones(palabraAleatoria);
-
-    return palabraAleatoria
+  return palabraAleatoria;
 };
 
-export {palabraSecreta};
+const validarTecla = (palabra, mayuscula, minuscula) => {
+  if (palabra.match(mayuscula) || palabra.match(minuscula)) {
+    
+    return true;
+  } else {
+    return false;
+  }
+};
+
+function nuevaPalabra() {
+  let valor = document.querySelector(".texta-agregar-palabra").value;
+  console.log(valor);
+  palabras.push(valor);
+}
+
+export {palabraSecreta, validarTecla, nuevaPalabra};
